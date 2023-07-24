e937c544851bb4a424a862a91bd56d095b7946e7
badtripllama <72275683+badtripllama@users.noreply.github.com>
2023-07-24 13:49:05 +0400

    extraction for failed sign ins, no date

---
+++ 1.ps1
@@ -0,0 +1,14 @@
+Import-Module AzureADPreview
+
+Connect-AzureAD
+
+
+
+$founduserstats = Get-AzureADAuditSignInLogs -Filter "status/errorCode ne 0" | `
+Select UserDisplayName, AppDisplayName, IpAddress, Status, DeviceDetail, Location, MfaDetail
+
+
+$userstats = $userstats + $founduserstats
+
+
+$userstats | Export-Csv -Path "C:\Users\jramphul\Desktop\signins.csv" -notype
